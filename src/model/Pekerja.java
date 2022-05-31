280aae1fe522adf81fa9c14e8a13f77725a2619f
AdnSmile <nessafitri16@gmail.com>
2022-05-31 16:07:26 +0700

    membuat Pekerja.java

---
+++ src/model/Pekerja.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author nessa
+ */
+public class Pekerja {
+    private String nama;
+
+    public Pekerja(String nama) {
+        this.nama = nama;
+    }
+    
+    
+    
+}
